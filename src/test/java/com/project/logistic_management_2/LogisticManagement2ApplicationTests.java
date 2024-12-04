0e531b528ca864231d92216081df3a10ffe38cb2
trung381 <130747543+Trung381@users.noreply.github.com>
2024-12-04 12:58:00 +0700

    Initial commit

---
+++ src/test/java/com/project/logistic_management_2/LogisticManagement2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.logistic_management_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticManagement2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
