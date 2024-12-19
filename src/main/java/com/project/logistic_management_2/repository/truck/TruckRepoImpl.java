79c687f5fae8c02b9ff86a3a7e2a39e546c59c2b
Giang <140945898+dauvangiang@users.noreply.github.com>
2024-12-19 12:06:39 +0700

    fix: fix truck crud

--- src/main/java/com/project/logistic_management_2/repository/truck/TruckRepoImpl.java
+++ src/main/java/com/project/logistic_management_2/repository/truck/TruckRepoImpl.java
@@ -1,9 +1,10 @@
 package com.project.logistic_management_2.repository.truck;
 
-import com.project.logistic_management_2.entity.QTruck;
-import com.project.logistic_management_2.entity.Truck;
+import com.project.logistic_management_2.dto.truck.TruckDTO;
 import com.project.logistic_management_2.repository.BaseRepo;
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.ConstructorExpression;
+import com.querydsl.core.types.Projections;
 import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
 import jakarta.validation.constraints.NotNull;
@@ -13,48 +14,69 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
+import static com.project.logistic_management_2.entity.QTruck.truck;
+import static com.project.logistic_management_2.entity.QUser.user;
+
 @Repository
 public class TruckRepoImpl extends BaseRepo implements TruckRepoCustom {
     public TruckRepoImpl(EntityManager entityManager) {
         super(entityManager);
     }
 
-    @Override
-    public Optional<Truck> getTruckById(Integer id) {
-        QTruck qTruck = QTruck.truck;
-
-        BooleanBuilder builder = new BooleanBuilder();
-        builder.and(qTruck.id.eq(id));
-        builder.and(qTruck.deleted.eq(false));// Chỉ lấy bản ghi chưa bị xóa
-        Truck truck = query.from(qTruck)
-                .where(builder)
-                .select(qTruck)
-                .fetchOne();
-        return Optional.ofNullable(truck);
+    private ConstructorExpression<TruckDTO> expression() {
+        return Projections.constructor(TruckDTO.class,
+                truck.id.as("id"),
+                truck.licensePlate.as("licensePlate"),
+                truck.capacity.as("capacity"),
+                truck.driverId.as("driverId"),
+                user.fullName.as("driverName"),
+                truck.type.as("type"),
+                truck.status.as("status"),
+                truck.note.as("note"),
+                truck.createdAt.as("createdAt"),
+                truck.updatedAt.as("updatedAt")
+        );
     }
 
     @Override
-    public Optional<Truck> getTruckByLicensePlate(String licensePlate) {
-        QTruck qTruck = QTruck.truck;
+    public Optional<TruckDTO> getTruckById(Integer id) {
         BooleanBuilder builder = new BooleanBuilder();
-        builder.and(qTruck.licensePlate.eq(licensePlate));
-        builder.and(qTruck.deleted.eq(false));
-        Truck truck = query.from(qTruck)
-                .where(builder)
-                .select(qTruck)
-                .fetchOne();
-        return Optional.ofNullable(truck);
+        builder.and(truck.id.eq(id));
+        builder.and(truck.deleted.eq(false));// Chỉ lấy bản ghi chưa bị xóa
+
+        return Optional.ofNullable(
+                query.from(truck)
+                        .innerJoin(user).on(truck.driverId.eq(user.id))
+                        .where(builder)
+                        .select(expression())
+                        .fetchOne()
+        );
     }
 
     @Override
-    public List<Truck> getAllTrucks() {
-        QTruck qTruck = QTruck.truck;
-
+    public Optional<TruckDTO> getTruckByLicensePlate(String licensePlate) {
         BooleanBuilder builder = new BooleanBuilder();
-        builder.and(qTruck.deleted.eq(false));
-        return query.from(qTruck)
+        builder.and(truck.licensePlate.eq(licensePlate));
+        builder.and(truck.deleted.eq(false));
+
+        return Optional.ofNullable(
+                query.from(truck)
+                        .innerJoin(user).on(truck.driverId.eq(user.id))
+                        .where(builder)
+                        .select(expression())
+                        .fetchOne()
+        );
+    }
+
+    @Override
+    public List<TruckDTO> getAllTrucks() {
+        BooleanBuilder builder = new BooleanBuilder();
+        builder.and(truck.deleted.eq(false));
+
+        return query.from(truck)
+                .innerJoin(user).on(truck.driverId.eq(user.id))
                 .where(builder)
-                .select(qTruck)
+                .select(expression())
                 .fetch();
     }
 
@@ -62,14 +84,13 @@ public class TruckRepoImpl extends BaseRepo implements TruckRepoCustom {
     @Modifying
     @Transactional
     public long delete(Integer id) {
-        QTruck qTruck = QTruck.truck;
         BooleanBuilder builder = new BooleanBuilder()
-                .and(qTruck.id.eq(id))
-                .and(qTruck.deleted.eq(false));
+                .and(truck.id.eq(id))
+                .and(truck.deleted.eq(false));
 
-        return query.update(qTruck)
+        return query.update(truck)
                 .where(builder)
-                .set(qTruck.deleted, true)
+                .set(truck.deleted, true)
                 .execute();
     }
 
@@ -80,35 +101,31 @@ public class TruckRepoImpl extends BaseRepo implements TruckRepoCustom {
         if (status < -1 || status > 1) {
             return 0;
         }
-        QTruck qTruck = QTruck.truck;
-
         BooleanBuilder builder = new BooleanBuilder()
-                .and(qTruck.licensePlate.eq(license))
-                .and(qTruck.deleted.eq(false));
+                .and(truck.licensePlate.eq(license))
+                .and(truck.deleted.eq(false));
 
-        return query.update(qTruck)
+        return query.update(truck)
                 .where(builder)
-                .set(qTruck.status, status)
+                .set(truck.status, status)
                 .execute();
     }
 
-    public List<Truck> getTrucksByType(Integer type) {
-        // Tạo một đối tượng QTruck (QueryDSL) để xây dựng các truy vấn
-        QTruck qTruck = QTruck.truck;
-
+    public List<TruckDTO> getTrucksByType(Integer type) {
         // Tạo một BooleanBuilder để xây dựng các điều kiện lọc trong truy vấn
         BooleanBuilder builder = new BooleanBuilder();
 
         // Thêm điều kiện lọc: cột "type" phải bằng giá trị "type" được truyền vào
-        builder.and(qTruck.type.eq(type)); // Lọc theo giá trị của type (ví dụ: 0 hoặc 1)
+        builder.and(truck.type.eq(type)); // Lọc theo giá trị của type (ví dụ: 0 hoặc 1)
 
         // Thêm điều kiện lọc: chỉ lấy các bản ghi chưa bị xóa (cột "deleted" = 1)
-        builder.and(qTruck.deleted.eq(false)); // Lọc các bản ghi chưa bị xóa (deleted = false)
+        builder.and(truck.deleted.eq(false)); // Lọc các bản ghi chưa bị xóa (deleted = false)
 
         // Sử dụng QueryDSL để thực hiện truy vấn
-        return query.from(qTruck)               // Chọn bảng Truck làm nguồn truy vấn
-                .where(builder)            // Áp dụng các điều kiện lọc đã xây dựng
-                .select(qTruck)            // Chọn tất cả các cột trong bảng Truck
-                .fetch();                  // Thực thi truy vấn và trả về danh sách kết quả
+        return query.from(truck)
+                .innerJoin(user).on(truck.driverId.eq(user.id))
+                .where(builder)
+                .select(expression())
+                .fetch();
     }
 }
