1fec7424ce610e8ed35d1ac36c261fe540720255
Giang <140945898+dauvangiang@users.noreply.github.com>
2024-12-27 11:17:04 +0700

    fix: cors config

--- src/main/java/com/project/logistic_management_2/config/WebConfig.java
+++ src/main/java/com/project/logistic_management_2/config/WebConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
 import org.springframework.lang.NonNullApi;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -25,5 +26,15 @@ public class WebConfig implements WebMvcConfigurer {
         // URL bắt đầu bằng /uploads/ sẽ trỏ đến thư mục upload trên hệ thống file
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations(resourceLocation);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Áp dụng CORS cho tất cả các path
+                .allowedOrigins("http://localhost:3000") // Các domain được phép truy cập
+                .allowedMethods("*") // Các HTTP method được phép
+                .allowedHeaders("*") // Các header được phép (có thể tùy chỉnh cụ thể)
+                .allowCredentials(true) // Cho phép gửi cookie và credentials
+                .maxAge(3600); // Thời gian cache preflight request (giây)
     }
 }
