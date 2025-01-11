c4da0d67ac5b3260e91b9a9ffa5830f8c1dc806a
Giang <140945898+dauvangiang@users.noreply.github.com>
2025-01-11 09:55:54 +0700

    refactor: convert to date of timstamp

--- src/main/java/com/project/logistic_management_2/utils/Utils.java
+++ src/main/java/com/project/logistic_management_2/utils/Utils.java
@@ -1,11 +1,17 @@
 package com.project.logistic_management_2.utils;
 
 import com.project.logistic_management_2.enums.IDKey;
+import com.project.logistic_management_2.exception.def.InvalidParameterException;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.regex.Pattern;
 
 @Component
 public class Utils {
@@ -16,5 +22,44 @@ public class Utils {
         String code = now.format(formatter);
         int randomPart = ThreadLocalRandom.current().nextInt(1000, 9999);
         return key.label + code + randomPart;
+    }
+
+    static Date parseFromYearMonth(String yearMonthString) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM");
+        try {
+            LocalDate localDate = LocalDate.parse(yearMonthString, formatter);
+            return java.sql.Date.valueOf(localDate);
+        } catch (DateTimeParseException ex) {
+            throw new InvalidParameterException("Định dạng chu kỳ không hợp lệ! (yyyyy-MM)");
+        }
+    }
+
+    public static Date convertToDateOfCurrentMonth(String yearMonth) {
+        if (yearMonth == null) return null;
+        return parseFromYearMonth(yearMonth);
+    }
+
+    public Date convertToDateOfNextMonth(String yearMonth) {
+        Date date = convertToDateOfCurrentMonth(yearMonth);
+        return moveToNextMonth(date);
+    }
+
+    static Date moveToNextMonth(Date date) {
+        if (date == null) return null;
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.MONTH, 1);
+        return calendar.getTime();
+    }
+
+    public static Date convertToDateOfTimestamp(String dateString) {
+        if (dateString == null) return null;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        try {
+            LocalDate localDate = LocalDate.parse(dateString, formatter);
+            return java.sql.Date.valueOf(localDate);
+        } catch (DateTimeParseException ex) {
+            throw new InvalidParameterException("Định dạng ngày tháng không hợp lệ! (yyyy-MM-dd)");
+        }
     }
 }
